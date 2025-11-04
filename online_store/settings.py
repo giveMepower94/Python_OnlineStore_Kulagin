a426cd4a0751772c8ac92170b549370fa8f77d03
jGYQrw gga <Kul.vova2009@yandex.ru>
2025-11-05 01:42:19 +0300

    settings db postgres

--- online_store/settings.py
+++ online_store/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'online_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'online_store',
+        'USER': 'Kulagin',
+        'PASSWORD': 'Admin1994',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
