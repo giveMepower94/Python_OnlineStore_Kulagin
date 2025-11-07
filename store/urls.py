8b3a6289d3483d1cfd9901e4edcfea86ed4a68c9
jGYQrw gga <Kul.vova2009@yandex.ru>
2025-11-08 01:54:09 +0300

    add view stock_list

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView
+from .views import (ProductListView, 
+                    ProductDetailView,
+                    StockListView,
+                    StockDetailView)
 
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('stock/', StockListView.as_view(), name='stock_list'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from .models import Product
+from .models import Product, Stock
 
 
 # Create your views here.
@@ -13,3 +13,18 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = 'store/product_detail.html'
     context_object_name = 'product'
+
+
+class StockListView(ListView):
+    model = Stock
+    template_name = 'store/stock_list'
+    context_object_name = 'stocks'
+    
+    def get_queryset(self):
+        return Stock.objects.all()
+
+
+class StockDetailView(DetailView):
+    model = Stock
+    template_name = 'store/stock_detail'
+    context_object_name = 'stock'
