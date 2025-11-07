c2b4f59036c2ecef37b9ba447ef543ca8839b1e1
jGYQrw gga <Kul.vova2009@yandex.ru>
2025-11-08 01:09:20 +0300

    add view product_list

--- store/views.py
+++ store/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Product
+
 
 # Create your views here.
+class ProductListView(ListView):
+    model = Product
+    template_name = 'store/product_list.html'
+    context_object_name = 'products'
