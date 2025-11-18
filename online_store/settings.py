6cc6e8f8c82e6a4f9ba1919627f0e9b3ec33c581
jGYQrw gga <Kul.vova2009@yandex.ru>
2025-11-19 01:26:32 +0300

    users auth

--- online_store/settings.py
+++ online_store/settings.py
@@ -124,9 +124,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT='static/'
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'profile'
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = 'login'
